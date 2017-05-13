8344d9258ebd7b41cb3ae424664acd549b209b1e
Mario Vitale <mvitale1989@hotmail.com>
2017-05-13 15:55:22 +0200

    Migrated from lineinfile to blockinfile
    - Migrated from "lineinfile" module to "blockinfile", to prevent
      deletion and rewrite of managed lines at every ansible-playbook
      call.
    - Added parameter "ih_host_name", to map host names in inventory to
      arbitrary names, before addition to the hosts files.
    - Added parameter "ih_extra_hosts", to add further lines to the hosts
      file.
    - Added parameter "ih_clear", used to remove managed lines
      completely. Supersedes the previous shell script.
    - Handled IP case: if an IP is used directly in the inventory, it
      will be skipped during hosts file population.
    - Handled unavailable hosts case: if a host is not present in the
      playbook, a DNS query will be used to obtain its IP, instead of
      stil trying to rely on its facts.

--- files/remove_hosts_lines.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-cd $(dirname $0)
-IH_SIGNATURE=$(grep "ih_signature" ../defaults/main.yml | cut -d\" -f2)
-ansible all --become-method=sudo -m raw -a "sed -i"_bak" '/${IH_SIGNATURE}/d' /etc/hosts"
